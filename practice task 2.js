57417b1330c9796fdc9bb8586156334a71d147fa
jnakht <jisan.cse.cu@gmail.com>
2025-06-19 11:29:57 +0600

    practice task 2, problem 4 done, issue on prob 5,6

--- practice task 2.js
+++ practice task 2.js
@@ -29,6 +29,64 @@ const collection = db.massiveData;
 // ])
 
 // problem 4 
+// collection.aggregate([
+//     { $unwind : "$friends" },
+//     { $match : { "friends.name" : /^W/ } },
+//     { $group : { _id : null, uniqueFriends : { $addToSet : "$friends.name"} } }
+// ]) 
+
+
+// problem 5 , not solved yet
+// collection.aggregate([
+//     { $facet : {
+//         // pipeline 1 
+//         "ageBelowThirty" : [
+//             { $match : { age : { $lte : 30 } } },
+//             { $bucket: {
+//                   groupBy: "$age",
+//                   boundaries: [0, 10, 20, 30 ],
+//                   default: "ageOnThirty",
+//                   output: {
+//                     individuals : { $push : "$$ROOT" },
+//                     count : { $sum : 1 }
+//                   }
+//                 }},
+//                 { $unwind : "$individuals" },
+//                 { $sort : { "individuals.age" : 1 } },
+//                 { $group : { _id : "_id",} }
+                
+//         ],
+//         "ageAboveThirty": [
+//             { $match : { age : { $gt : 30 } } }, 
+//             { $bucket: {
+//                   groupBy: "$age",
+//                   boundaries: [ 31, 41, 51, 61, 71, 81, 100, 200 ],
+//                   default: "other",
+//                   output: {
+//                     "count": { $sum: 1 }
+//                   }
+//                 }},
+//                 { $sort : { age : 1 } }
+//         ]
+//     }}    
+// ])
+
+
+// problem 6 
+collection.aggregate([
+    
+    { $group : { _id : "company", totalBalance : { $sum : "$balance" } } }    
+])
+
+
+
+
+
+
+
+
+
+
 
 
 
