75577b309539e6737dc78033527c75f5397d29f2
jnakht <jisan.cse.cu@gmail.com>
2025-07-19 14:35:16 +0600

    module 15.10 recap

---
+++ Module 15 Recap/3.js
@@ -0,0 +1,47 @@
+
+
+
+const collection = db.getCollection("test");
+
+// collection.updateOne( 
+//     { _id :  ObjectId("6406ad63fc13ae5a40000069")}, 
+//     { $set : {
+//         "address.city" : "Dhaka"
+//     }}
+// )
+
+
+// collection.updateOne( 
+//     { _id :  ObjectId("6406ad63fc13ae5a40000069")}, 
+//     { $set : {
+//         "address.city" : "Rangpur",
+//         "address.postalCode" : "5631",
+//         "address.country" : "Bangladesh"
+//     }}
+// )
+
+
+// collection.updateOne( 
+//     { _id :  ObjectId("6406ad63fc13ae5a40000069"), "skills.name" : "JAVASCRIPT" },
+//     { $set : {
+//         "skills.$.level" : "Newbie"
+//     }}
+// )
+
+// collection.updateOne( 
+//     { _id :  ObjectId("6406ad63fc13ae5a40000069")},
+//     { $inc : {
+//         salary : 1
+//     }}
+// )
+
+
+// collection.updateOne(
+//     { _id :  ObjectId("6406ad63fc13ae5a40000069") }, 
+//     { $inc : {
+//         salary : 500
+//     }}
+// )
+
+
+
